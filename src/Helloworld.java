bd14d831522eef03e8cecca579b49b56c7eec029
Neria Fay Arinda <neriafay.a@gmail.com>
2025-01-10 10:51:49 -0800

    Added an error

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -1,3 +1,6 @@
-public class Helloworld
-{This is my first class
+public class Helloworld {
+    // This is my first class
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
