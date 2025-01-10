6a70a59fea491f5c6cef9c2d6dfcad586ea00621
Neria Fay Arinda <neriafay.a@gmail.com>
2025-01-10 10:55:50 -0800

    Update Helloworld.java

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -1,6 +1,6 @@
 public class Helloworld {
     // This is my first class
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, my World!");
     }
 }
